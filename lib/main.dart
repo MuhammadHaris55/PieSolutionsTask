06f32a2ea724667926b2e68ded90872901aa8deb
MuhammadHaris55 <hkhn567@gmail.com>
2021-06-07 00:37:24 +0500

    fetching data from api and showing list

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -17,14 +17,11 @@ class _MyAppState extends State<MyApp> {
   Map data;
   List _notes;
 
-  fetchAlbum () async {
+  fetchAlbum() async {
     var url = 'https://pietest.dev.new.wf/data.json';
     try {
-
-      var response = await http.get(
-          Uri.parse(url),
-
-          headers: {"content-type": "application/json"});
+      var response = await http
+          .get(Uri.parse(url), headers: {"content-type": "application/json"});
       print(response.statusCode);
       if (response.statusCode == 200) {
         print(response.body);
@@ -33,38 +30,49 @@ class _MyAppState extends State<MyApp> {
           _notes = data['categories'];
         });
 
-
         print('after decode');
-
-
       }
-    }catch(e) {
+    } catch (e) {
       print(e);
     }
   }
-    @override
-    void initState() {
-      super.initState();
-      fetchAlbum();
-    }
 
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'Fetching Data',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
+  @override
+  void initState() {
+    super.initState();
+    fetchAlbum();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fetching Data',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
           appBar: AppBar(
             title: Text('PieSolutionsTask'),
           ),
-          body: _notes == null ? Center(child: Text("Data is loading")) : ListView.builder(
-              itemCount: _notes.length,
-              itemBuilder: (context, int index){
-                return Center(child: Text(_notes[index]['name']));
-              })
-          ),
-        );
+          body: _notes == null
+              ? Center(child: Text("Data is loading"))
+              : ListView.builder(
+                  itemCount: _notes.length,
+                  itemBuilder: (context, int index) {
+                    return Center(
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 20.0),
+                        margin: EdgeInsets.only(top: 10.0),
+                        decoration: BoxDecoration(
+                          border: Border.all(),
+                        ),
+                        width: MediaQuery.of(context).size.width / 1.1,
+                        child: Text(
+                          _notes[index]['name'],
+                        ),
+                      ),
+                    );
+                  })),
+    );
   }
-}// class MyHomePage extends StatefulWidget {
-
+} // class MyHomePage extends StatefulWidget {
